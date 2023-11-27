2954b8b86ac6819d1a5ff8acf88163af3e51d2b5
mouram chafikE <mrmouramito@gmail.com>
2023-11-27 11:06:49 +0100

    update code test

--- tests/ movies.test.js
+++ tests/ movies.test.js
@@ -12,19 +12,19 @@ describe("GET /api/movies", () => {
 
 describe("GET /api/movies/:id", () => {
     // Pour l'id 1, le test vérifie le statut réponse 200
-    it("should return all movies id", async () => {
+    it("should return movie id 1", async () => {
         const response = await request(app).get('/api/movies/1');
         expect(response.status).toBe(200);
     });
 
     // Pour l'id 1, le test vérifie le format de réponse en json
-    it("should return all movies id", async () => {
+    it("should return movie id 1 type json", async () => {
         const response = await request(app).get("/api/movies/1");
         expect(response.headers["content-type"]).toMatch(/json/);
       });
 
       // Pour l'id 0, le test vérifie le status de réponse 404 (Not Found)
-      it("should return all movies id", async () => {
+      it("should return no movie", async () => {
         const response = await request(app).get('/api/movies/0');
         expect(response.status).toBe(404);
       });
